4ad67e2b0848db97b23a3dd41474573394f51c0a
prasoonbadola <prasoonbadola@gmail.com>
2022-04-18 16:16:34 +0530

    added express validator in users route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const {check,validationResult} = require('express-validator')
 
-//@route GET api/users
-//@des TEST route
+
+//@route POST api/users
+//@des register user route
 //@access Public
-router.get('/',(req,res)=>{
-    res.send('user route')
+router.post('/',[
+    check('name','Name is required').not().isEmpty(),
+    check('email', 'Please enter correct email address').isEmail(),
+    check('password', 'Please enter password with 6 or more characters').isLength({min:6}),
+
+],(req,res)=>{
+    try {
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
+        }
+
+        console.log(req.body);
+    } catch (error) {
+        console.error(error.message);
+    }
 })
 
 
