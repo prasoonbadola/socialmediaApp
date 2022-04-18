e40192f1dc0b2119f03b876a32ca14bb185b3f3f
prasoonbadola <prasoonbadola@gmail.com>
2022-04-18 17:45:07 +0530

    created get profile api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
 
-//@route GET api/users
-//@des TEST route
-//@access Public
-router.get('/', (req, res) => {
-    res.send('profile route')
+//@route GET api/profile/me
+//@des  get current user profile
+//@access Private
+router.get('/me', auth, async(req, res) => {
+    try {
+        const profile = await Profile.findOne({user:req.user.id}).populate('user',['name','avatar'])
+
+        if(!profile)
+        {
+            return res.status(400).send('no profile for this user');
+        }
+
+        res.json(profile);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('server error: ')
+    }
 })
 
 
